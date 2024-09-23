25274aa89ef6f5feb31d0d79b59a5804467c215b
unknown <k.rezonova@gost.team>
2024-09-23 15:51:13 +0300

    Добавила домашку1

---
+++ задание 1.py
@@ -0,0 +1,3 @@
+char = input('Введите символ: ')
+for i in range(1,6):
+   print(char * i)
---
+++ задание 2.py
@@ -0,0 +1,27 @@
+month_number = int(input("Введите номер месяца: "))
+if month_number == 1:
+    print("Январь")
+elif month_number == 2:
+    print("Февраль")
+elif month_number == 3:
+    print("Март")
+elif month_number == 4:
+    print("Апрель")
+elif month_number == 5:
+    print("Май")
+elif month_number == 6:
+    print("Июнь")
+elif month_number == 7:
+    print("Июль")
+elif month_number == 8:
+    print("Август")
+elif month_number == 9:
+    print("Сентябрь")
+elif month_number == 10:
+    print("Октябрь")
+elif month_number == 11:
+    print("Ноябрь")
+elif month_number == 12:
+    print("Декабрь")
+else:
+    print("Введите число от 1 до 12")
